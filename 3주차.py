2596862bb71d30ac9c7e1d6143f7999391e0fd44
EHDFUF <goahtn2317@naver.com>
2025-03-26 16:45:31 +0900

    3_weeks_sub

--- 3주차.py
+++ 3주차.py
@@ -1,11 +1,20 @@
-# temp(i, i) i = 진수변환을 위해 이렇게 양식 해야함
-
 # 16진수변환함수 hex()
 # 8진수변환함수 oct()
 # 2진수변환함수 bin()
 
+number_decide = int(input("입력 진수 결정(16/10/8/2) : "))
 
-number_ = int(input("입력 진수 설정(16/10/8/2) : "))
+if number_decide == 16 or number_decide == 10 or number_decide == 8 or number_decide == 2:
 
+    number_input = input("값 입력 : ")
+    number_for_convert = int(number_input, number_decide) #hex(),oct(),bin() 함수를 사용하기 위해 number_input을 10진수 형태로 전환
+
+    print(f"16진수 ==> {hex(number_for_convert)}")
+    print(f"10진수 ==> {number_for_convert}")
+    print(f"8진수 ==> {oct(number_for_convert)}")
+    print(f"2진수 ==> {bin(number_for_convert)}")
+
+else:
+    print("16/10/8/2 중의 숫자로 작성하세요")
 
 
